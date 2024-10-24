0d64f5463506ecb57d20f5c6b51544308c3049b8
Joey <babykangaroo117@gmail.com>
2024-10-24 08:29:18 -0400

    Create a process for each file

--- Project-1/word-frequency.c
+++ Project-1/word-frequency.c
@@ -9,3 +9,34 @@
 #include <sys/wait.h>
 #include <string.h>
 
+// Project Params
+#define NUM_FILES       7
+#define MAX_FILE_LENGTH 10
+
+int main () {
+
+    
+    char filePaths[NUM_FILES][MAX_FILE_LENGTH] = {"bib", "paper1", "paper2", "progc", "progl", "progp", "trans"};
+    
+    // Create process for each file
+
+    pid_t pid;
+    for (int i = 0; i < NUM_FILES; i++) {
+        pid = fork();
+        if (pid < 0) {
+            printf("Process creation failed");
+        }
+        // Child processes
+        else if (pid == 0) {
+            
+
+            // Child process shouldn't create any more threads
+            return 0;
+        }
+
+        // Parent process keeps creating more child processes
+    }
+
+    return 0;
+
+}
